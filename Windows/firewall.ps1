d1a43dc85892d010ae7d0f3f809f2ffa5048110e
Charles Pelletier <cjpelletier3.ftw@gmail.com>
2022-10-27 13:40:11 -0500

    corrected CCT port

--- Windows/firewall.ps1
+++ Windows/firewall.ps1
@@ -1,5 +1,5 @@
 #Set-ExecutionPolicy RemoteSigned -Force
-New-NetFirewallRule -DisplayName 'PAR Pay (CCT)' -Profile @('Domain','Private','Public') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('8003')
+New-NetFirewallRule -DisplayName 'PAR Pay (CCT)' -Profile @('Domain','Private','Public') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('8030')
 New-NetFirewallRule -DisplayName 'PAR Pay (Pay @ Table)' -Profile @('Domain','Private','Public') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('9000')
 New-NetFirewallRule -DisplayName 'PAR Pay (POS)' -Profile @('Domain','Private','Public') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('8001')
 New-NetFirewallRule -DisplayName 'PAR Pay (SDK)' -Profile @('Domain','Private','Public') -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('15583')
